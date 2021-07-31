db4e9f1ca6625c8779016d2862fe3f689c647336
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-07-31 22:05:09 +0800

    Migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\SubCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +18,64 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // $this->call(UsersTableSeeder::class);
+        Category::create(['name'=>'laptop','slug'=>'laptop','description'=>'laptop category','image'=>'files/photo1.jpeg']);
+        Category::create(['name'=>'mobile phone','slug'=>'mobile-phone','description'=>'mobile phone category','image'=>'files/photo1.jpeg']);
+
+         Category::create(['name'=>'books','slug'=>'books','description'=>'bookx category','image'=>'files/photo1.jpeg']);
+
+        SubCategory::create(['name'=>'dell','category_id'=>1]);
+        SubCategory::create(['name'=>'hp','category_id'=>1]);
+        SubCategory::create(['name'=>'lenovo','category_id'=>1]);
+
+
+        Product::create([
+        	'name'=>'HP LAPTOPS ',
+        	'image'=>'product/yyO8bETsNup8nECU8sF0d7VBeRVbkEiQWthswpHP.jpeg',
+        	'price'=> rand(700,1000),
+        	'description'=>'This is the description of a product',
+        	'additional_info'=>'This is additional info',
+        	'category_id'=> 1,
+            'subcategory_id'=>1
+
+
+
+        ]);
+
+        Product::create([
+        	'name'=>'Dell LAPTOPS ',
+        	'image'=>'product/D0mNLGw8VW0pXZHRAzHKNACD7b0QaMQa62ptAEjD.jpeg',
+        	'price'=> rand(800,1000),
+        	'description'=>'This is the description of a product',
+        	'additional_info'=>'This is additional info',
+        	'category_id'=> 1,
+            'subcategory_id'=>1
+
+
+
+
+        ]);
+
+        Product::create([
+        	'name'=>'LENOVO LAPTOPS ',
+        	'image'=>'product/tjPjUNJBnugQkGiSFSCEhCvO23V7Rqz7RmzXkUNV.jpeg',
+        	'price'=> rand(700,1000),
+        	'description'=>'This is the description of a product',
+        	'additional_info'=>'This is additional info',
+        	'category_id'=> 1,
+            'subcategory_id'=>2
+
+
+
+        ]);
+        User::create([
+        	'name'=>'LaraAdmin',
+        	'email'=>'admin@gmail.com',
+        	'password'=>bcrypt('password'),
+        	'email_verified_at'=>NOW(),
+        	'address'=>'Australia',
+        	'phone_number'=>'0576232',
+        	'is_admin'=>1
+        ]);
     }
 }
